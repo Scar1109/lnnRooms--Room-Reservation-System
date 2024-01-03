bb0cc3231b473f93430338ea6139584b9b5ec60f
Kaveen Dinethma <kaveen.dinethma@gmail.com>
2024-01-03 17:25:00 +0530

    updates

--- client/src/screens/HomeScreen.js
+++ client/src/screens/HomeScreen.js
@@ -1,6 +1,7 @@
 import React, {useEffect,useState} from 'react'
 import axios from 'axios';
 import Room from '../components/Room';
+import BeatLoader from "react-spinners/BeatLoader";
 function HomeScreen() {
 
     const [rooms, setRooms] = useState([]);
@@ -25,14 +26,14 @@ function HomeScreen() {
 
     return (
         <div className='row justify-content-center mt-4'>
-            {loading ? <div className ="spinner-border" role="status" style={{marginTop : "370px"}}>
-                    
-                </div> : error ? <p>An error happen please try again</p> : rooms.map( room => {
+            {loading ? <div className='spinner'><BeatLoader
+                color="#000000"
+                size={15}
+                speedMultiplier={0.6}
+                /></div> : error ? <p>An error happen please try again</p> : rooms.map( room => {
                     return<div className='col-md-9 mt-2'>
                             <Room room = {room} />
                         </div>
-    
-                    
                 })}
         </div>
     )
