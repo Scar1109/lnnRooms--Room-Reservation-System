6ed3c81e4a04ddcc4c0d709dcf9470e84912a298
Kaveen Dinethma <kaveen.dinethma@gmail.com>
2024-01-09 03:49:01 +0530

    Update Profile.js

--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -1,9 +1,47 @@
 import React from "react";
+import { Tag } from "antd";
 
 function Profile() {
-    return <div>
-        
-    </div>;
+    const user = JSON.parse(localStorage.getItem("currentUser"));
+    console.log(user);
+    return (
+        <div className="text-start">
+            <h2 className="mt-1 ">My Profile</h2>
+            <div style={{ fontSize: "1.2rem" }} className="mt-3">
+                <p>First Name : {user.firstName}</p>
+                <p>Last Name : {user.lastName}</p>
+                <p>Email : {user.email}</p>
+                <p>
+                    Profile Status :{" "}
+                    {user.isAdmin ? (
+                        <Tag color="red">ADMIN</Tag>
+                    ) : (
+                        <Tag color="green">USER</Tag>
+                    )}
+                </p>
+                <button
+                    className="btn btn-primary m-3"
+                    style={{
+                        backgroundColor: "#323232",
+                        boxShadow: "none",
+                        borderColor: "#323232",
+                    }}
+                >
+                    Edit Details
+                </button>
+                <button
+                    className="btn btn-primary m-3"
+                    style={{
+                        backgroundColor: "#323232",
+                        boxShadow: "none",
+                        borderColor: "#323232",
+                    }}
+                >
+                    Change Password
+                </button>
+            </div>
+        </div>
+    );
 }
 
 export default Profile;
