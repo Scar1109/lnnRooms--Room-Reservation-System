a6a139f1f84ad55bfce3f6003e5b598868339be3
Kaveen Dinethma <kaveen.dinethma@gmail.com>
2024-02-01 05:09:14 +0530

    IR | update delete API call

--- client/src/components/AdminRooms.jsx
+++ client/src/components/AdminRooms.jsx
@@ -29,7 +29,7 @@ function AdminRooms() {
     const deleteRoom = (roomId) => {
         try{
             setLoading(true);
-            axios.get(`/api/rooms/deleteRoom/${roomId}`);
+            axios.get(`${process.env.PUBLIC_URL}/api/rooms/deleteRoom/${roomId}`);
             setLoading(false);
             Swal.fire({
                 title: "Room Deleted Successfully",
